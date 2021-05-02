a362593fef8f6440b4164a62f1958921c14bf876
Mauricio Dias <mauriciocd7@gmail.com>
2021-05-02 18:18:26 -0300

    click event created on numbers

--- app/src/main/java/br/com/dio_calculator_kotlin/MainActivity.kt
+++ app/src/main/java/br/com/dio_calculator_kotlin/MainActivity.kt
@@ -2,10 +2,44 @@ package br.com.dio_calculator_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.dio_calculator_kotlin.R.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide() // remove a toolbar
+
+
+
+        //eventos de clique
+        numero_zero.setOnClickListener { AcrescentarUmaExpressao(string =  "0", limpar_dados = true)} //valores para os parâmetros
+
+
     }
+
+    fun AcrescentarUmaExpressao(string: String, limpar_dados: Boolean){
+
+        //validações
+        if(resultado.text.isNotEmpty()){ //resultado não estiver vazio
+            expressao.text = ""
+        }
+
+        if(limpar_dados){
+            resultado.text = ""
+            expressao.append(string) //acrescenta a string
+        }
+
+        else{
+            expressao.append(resultado.text)
+            expressao.append(string)
+            resultado.text = "" //limpa dados
+        }
+
+    }
+
+
 }
